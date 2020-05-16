e1e43f3bcb2785e94230f8ef34e22815ab68e680
Saul Pwanson <code@saul.pw>
2020-05-15 17:41:19 -0700

    checkpoint

--- main.c
+++ main.c
@@ -35,7 +35,28 @@ typedef struct array {
 #define A_i A->vals[i%$A]
 #define B_i B->vals[i%$B]
 
-// --- stack ---
+// --- dict ---
+char PAD[80];
+
+struct verb;
+typedef int (verbfunc)(struct verb *);
+typedef struct verb {
+    const char *name;
+    verbfunc *func;
+    struct verb *args[];
+} verb;
+extern verb __start_verbs[], __stop_verbs[];
+
+#define _VERB(TOK, VERBNAME, ARGS...) extern int v_##VERBNAME(verb *v); \
+  verb verb_##VERBNAME __attribute__((__section__("verbs"))) __attribute__((__used__)) = { TOK, v_##VERBNAME, {ARGS} }; \
+  int v_##VERBNAME(verb *v)
+
+verb *find(const char *tok) {
+    DO(__stop_verbs-__start_verbs, if(!strcasecmp(__start_verbs[i].name, PAD)) return &__start_verbs[i]);
+    return NULL;
+}
+
+// --- stacks and forth internals ---
 array *STACK[16];
 array **SP = STACK;
 #define DEPTH (SP-STACK)
@@ -43,25 +64,19 @@ array *push(array *a) { *SP++ = a; return a; }
 array *pop(void) { assert(DEPTH > 0); return *--SP; }
 #define peek(X) *(SP-(X)-1)
 
-char PAD[80];
 void cr(void) { PR("\n"); }
 
-// --- dict ---
-typedef int (verbfunc)(void);
-typedef struct verb {
-    const char *name;
-    verbfunc *func;
-} verb;
-extern verb __start_verbs[], __stop_verbs[];
+// --- return stack ---
+verb **RSTACK[16];
+verb ***RP = RSTACK;
+#define RDEPTH (RP-RSTACK)
+verb **rpush(verb **ptr) { *RP++ = ptr; return ptr; }
+verb **rpop(void) { assert(RDEPTH > 0); return *--RP; }
 
-#define _VERB(TOK, VERBNAME) extern int v_##VERBNAME(void); \
-  verb verb_##VERBNAME __attribute__((__section__("verbs"))) __attribute__((__used__)) = { TOK, v_##VERBNAME }; \
-  int v_##VERBNAME(void)
+verb **IP = NULL;
 
-verb *find(const char *tok) {
-    DO(__stop_verbs-__start_verbs, if(!strcasecmp(__start_verbs[i].name, PAD)) return &__start_verbs[i]);
-    return NULL;
-}
+const char *TIB = NULL;
+
 
 // --- arrays ---
 void copy(INT *d, INT *s, int n) { DO(n, d[i]=s[i]); }
@@ -106,21 +121,27 @@ int parse(char *out, const char *input) { // return number of chars parsed into 
     return s - input;
 }
 
-void interpret(char *input) {
-    while (*input) {
-        int i = parse(PAD, input);
-        if (!*PAD) break;
-        char *endptr = NULL;
-        i64 v = strtoll(PAD, &endptr, 0);
-        if (endptr != PAD) { // some valid number
-            append(peek(0), v);
-        } else {
-            verb *v = find(PAD);
-            if (!v) { PR("unknown: %s\n", PAD); }
-            else { v->func(); }
-        }
-        input += i;
+// parse single token from input buffer, and either parse+append number, or find+execute word
+// return -1 if more input needed.
+
+_VERB("interpret_word", interpret_word) {
+    if (!TIB) { return -1; }
+    if (!TIB[0]) { return -1; }
+
+    int i = parse(PAD, TIB);
+    if (!*PAD) return -1;
+
+    char *endptr = NULL;
+    i64 val = strtoll(PAD, &endptr, 0);
+    if (endptr != PAD) { // some valid number
+        append(peek(0), val);
+    } else {
+        verb *v = find(PAD);
+        if (!v) { PR("unknown: %s\n", PAD); }
+        else { v->func(v); }
     }
+    TIB += i;
+    return 1;
 }
 
 int main()
@@ -128,8 +149,8 @@ int main()
     char s[128];
     while(fgets(s, sizeof(s), stdin)) {
         PR("   DEPTH=%ld >>>  %s", DEPTH, s);
-        interpret(s);
-        if (DEPTH) print(peek(0));
+        TIB = s;
+        while (v_interpret_word(NULL) > 0) {}
     }
 }
 
@@ -150,3 +171,16 @@ UNOP(".", print, print(A))
 UNOP("??", check, DO1(A, assert(A_i)))
 VERB("reshape", reshape, B=pop(); A=peek(0); reshape(A, $B, B->vals))
 UNOP("*/", mult_reduce, int acc=1; DO($A, acc *= A_i); push(boxint(acc)))
+
+_VERB("ENTER", enter) { rpush(IP); IP = v->args; return 0; }
+_VERB("EXIT", exit)   { IP = rpop(); return 0; }
+_VERB("(BRANCH)", branch) { INT i=(INT) *IP++; IP += i; return 0; }
+
+#define CAT(a,b) a##b
+#define XCAT(a,b) CAT(a,b)
+#define DEF(TOK, ARGS...) \
+  verb XCAT(verb_, __LINE__) __attribute__((__section__("verbs"))) __attribute__((__used__)) = { TOK, v_enter, {ARGS} };
+
+
+DEF("QUIT", &verb_interpret_word, &verb_branch, (verb *) -3);
+DEF("SQUARE", &verb_dup, &verb_mult, &verb_exit)
